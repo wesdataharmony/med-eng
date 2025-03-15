d148e68e4e1c05be4df2edfad567ea2ab97dce56
wesdataharmony <wesley_luc@yahoo.com.br>
2025-03-15 20:35:50 -0300

    Correção na conexão do banco de dados

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,9 @@
 # Configurações do banco de dados
 DB_CONFIG_POSTGRES = {
-    'dbname': 'datafoundation',
+    'dbname': 'data',
     'user': 'postgres',
     'password': '00@000',
-    'host': '34.863.75.31',
+    'host': '00.000.00.00',
     'port': '6431',
     'schema': 'test_medical'
 },
