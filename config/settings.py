78c14a4f7273afbfaedb65f1494eb5b8191f93f2
wesdataharmony <wesley_luc@yahoo.com.br>
2025-03-30 17:07:53 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -2,29 +2,29 @@ import os
 
 # Configurações do banco de dados
 DB_CONFIG_POSTGRES = {
-    'dbname': 'data',
-    'user': 'postgres',
-    'password': '00@000',
-    'host': '00.000.00.00',
-    'port': '6431',
-    'schema': 'test_medical'
+    'dbname': 'medical',
+    'user': 'medic',
+    'password': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'schema': 'etl'
 }
 
 DB_CONFIG_SQLITE = {
     'database': 'medicaldatabase.db',  # Arquivo do SQLite
-    'schema': 'test_medical'           # Schema padrão do SQLite
+    'schema': 'elt'           # Schema padrão do SQLite
 }
 
 DB_CONFIG_POSTGRES_DOCKER = {
-    'dbname': os.getenv('POSTGRES_DB', 'data'),
-    'user': os.getenv('POSTGRES_USER', 'postgres'),
-    'password': os.getenv('POSTGRES_PASSWORD', '00@000'),
-    'host': os.getenv('POSTGRES_HOST', 'postgres'),  # Usar nome do serviço
+    'dbname': os.getenv('POSTGRES_DB', 'medical'),
+    'user': os.getenv('POSTGRES_USER', 'medic'),
+    'password': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+    'host': os.getenv('POSTGRES_HOST', 'localhost'),  # Usar nome do serviço
     'port': os.getenv('POSTGRES_PORT', '5432'),      # Porta padrão do PostgreSQL
-    'schema': os.getenv('POSTGRES_SCHEMA', 'test_medical')
+    'schema': os.getenv('POSTGRES_SCHEMA', 'etl')
 }
 
 DB_CONFIG_SQLITE_DOCKER = {
     'database': os.path.join(os.getenv('DATA_DIR', 'data'), 'medicaldatabase.db'),
-    'schema': os.getenv('SQLITE_SCHEMA', 'test_medical')
+    'schema': os.getenv('SQLITE_SCHEMA', 'etl')
 }
